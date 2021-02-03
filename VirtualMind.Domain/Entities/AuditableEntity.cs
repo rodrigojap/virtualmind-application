94b5fb9ffbacbca17c4c66a78b66c4fa950c31c4
Rodrigo José Alves Pires <rodrigo.alves170@gmail.com>
2021-02-03 10:01:33 -0300

    operation entity

---
+++ VirtualMind.Domain/Entities/AuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VirtualMind.Domain.Entities
+{
+    public abstract class AuditableEntity
+    {
+        public DateTime Created { get; set; }
+    }
+}
---
+++ VirtualMind.Domain/Entities/Operation.cs
@@ -0,0 +1,19 @@
+﻿using VirtualMind.Domain.Enums;
+
+namespace VirtualMind.Domain.Entities
+{
+    public class Operation : AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public decimal RequestedAmount { get; set; }
+
+        public decimal CurrentQuote { get; set; }
+
+        public decimal PurchasedAmount { get; set; }
+
+        public Currency Currency { get; set; }
+    }
+}
